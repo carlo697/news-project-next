bb072bce89e58cf7e6548aea2e2ba7f5ebb694f4
carlo697 <carlos.07888@gmail.com>
2023-01-29 17:42:07 -0400

    Add link to header

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const AppHeader = () => {
   return (
     <header className="sticky top-0 bg-white">
-      <div className="section-container flex justify-center">Header</div>
+      <div className="section-container flex justify-center py-3">
+        <h1 className="text-2xl md:text-4xl">
+          <Link href="/">Header</Link>
+        </h1>
+      </div>
     </header>
   );
 };
